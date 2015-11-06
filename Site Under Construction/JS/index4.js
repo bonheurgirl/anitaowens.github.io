55a5c0abddce63f055c3e2a224b6fa0c88adb200
Anita Owens <anitaowens@Anita-Owens-MacBook-Air.local>
2015-11-06 21:02:20 +0100

    Removing unnecessary files

--- Site Under Construction/JS/index4.js
+++
@@ -1,7 +0,0 @@
-jQuery(index4.html).ready(function () {
-    $('.button').on('click', function(){
-        $(".vernice").attr("class", "vernice verniceOver");
-        $(".brush").attr("class", "brush brushOver");
-        $(".gocce").attr("class", "gocce gocceOver");
-    });
-});
