565783904ff087d6807426d5a3995a267b010e9f
Anita Owens <anitaowens@Anita-Owens-MacBook-Air.local>
2015-12-21 10:31:11 +0100

    Added new code.

---
+++ hello_world.py
@@ -0,0 +1,33 @@
+"""print ("hello world")
+
+def add2(x,y):
+    return x+y
+print add2(1,1)
+
+def mult2(x,y):
+    if x < 100:
+        x=1
+    return x*y
+print mult2(100,99)
+"""
+
+str="<meta name= Distribution  content= Global  />"
+print len(str)
+
+
+
+str="<script type= text/javascript >"
+print len(str)
+
+
+
+
+str="  })();"
+print len(str)
+
+
+				
+				
+				
+str="				<ul class= sidemenu >"
+print len(str)
